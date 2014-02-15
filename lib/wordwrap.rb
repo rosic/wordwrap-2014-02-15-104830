e0c5934b801cd9fdf2ae178593a8590ab7e2f843
Marek Rosa <marek.rosa@gmail.com>
2014-02-15 13:36:09 +0100

    First run on this kata
    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━┓
    ┃ Requirement                                                                      ┃ Time     ┃
    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╊━━━━━━━━━━┫
    ┃ Create a "WordWrap" class that is initialized with an integer parameter          ┃ 00:02:41 ┃
    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╊━━━━━━━━━━┫
    ┃ Create a wrap method that takes a string parameter                               ┃ 00:04:55 ┃
    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╊━━━━━━━━━━┫
    ┃ It should wrap the empty string to an empty string                               ┃ 00:00:52 ┃
    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╊━━━━━━━━━━┫
    ┃ It should return one short word as-is                                            ┃ 00:01:25 ┃
    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╊━━━━━━━━━━┫
    ┃ It should wrap text at the last space before the wrap length                     ┃ 00:25:10 ┃
    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╊━━━━━━━━━━┫
    ┃ Long words without spaces should be broken at the wrap length                    ┃ 00:08:09 ┃
    ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╊━━━━━━━━━━┫
    ┃ Strings with long words and spaces should mix breaking words and at spaces       ┃ 00:17:40 ┃
    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━┛

--- lib/wordwrap.rb
+++ lib/wordwrap.rb
@@ -1,2 +1,29 @@
 class Wordwrap
+  attr_reader :wrap_length
+
+  def initialize(wrap_length)
+    raise "Must provide a number" unless wrap_length.is_a?(Integer)
+    @wrap_length = wrap_length
+  end
+
+  def wrap(text)
+    raise "Not a text" unless text.is_a?(String)
+
+    final = ''
+    line = ''
+
+    s = text.scan(%r{(\w{#{wrap_length}})|(\w+)(\s|$)}) do |match|
+      match = match[0] || match[1]
+
+      if line.length + match.length > wrap_length
+        final << line << "\n"
+        line = match
+      else
+        line << ' ' unless line.empty?
+        line << match
+      end
+    end
+
+    final << line
+  end
 end
--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -5,8 +5,66 @@ describe Wordwrap do
   describe "#initialize" do
     it "instantiates" do
       expect {
-        Wordwrap.new
+        Wordwrap.new(4)
       }.to_not raise_exception
     end
+
+    it "raises on empty arguments" do
+      expect {
+        Wordwrap.new
+      }.to raise_exception
+    end
+
+    it "raises on non-numeric argument" do
+      expect {
+        Wordwrap.new "foo"
+      }.to raise_exception
+    end
+  end
+
+  describe "#wrap" do
+    subject { Wordwrap.new(4) }
+
+    it "accepts string" do
+      expect {
+        subject.wrap("this is a text")
+      }.to_not raise_exception
+    end
+
+
+    it "raises for non-string parameters" do
+      expect { subject.wrap([1,2,3]) }.to raise_exception
+    end
+
+    it "wraps empty string" do
+      subject.wrap("").should == ""
+    end
+
+    it "returns one short word as-is" do
+      Wordwrap.new(6).wrap('word').should == 'word'
+    end
+
+    describe "wraps at the last space before wrap length" do
+      specify do
+        Wordwrap.new(5).wrap("word word").should == "word\nword"
+      end
+
+      specify do
+        Wordwrap.new(5).wrap("word word word").should == "word\nword\nword"
+      end
+
+      specify do
+        Wordwrap.new(10).wrap("word word word").should ==  "word word\nword"
+      end
+    end
+
+    specify "Long words without spaces should be broken at the wrap length" do
+      Wordwrap.new(4).wrap("wordword").should == "word\nword"
+    end
+
+    specify "Strings with long words and spaces should mix breaking words and at spaces" do
+      Wordwrap.new(3).wrap("word word word").should == "wor\nd\nwor\nd\nwor\nd"
+    end
+
   end
 end
